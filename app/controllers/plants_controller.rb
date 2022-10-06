97452ee6c2d91068f176f75b8e6be32ae86bc3ed
Olivia Adongo <akinyioliviatove@gmail.com>
2022-10-06 18:10:06 +0300

    commit tree 64b47f40a4be1b6828281595bdc4bd823cb64f37

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -16,8 +16,29 @@ class PlantsController < ApplicationController
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
+  end 
+  def update
+     plant = Plant.find_by(id: params[:id])
+     if plant
+      plant.update(plant_params)
+      render json: plant
+     else
+      render json: {error: "Could not find plant"}, status: :not_found
+     end
+    
   end
 
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+
+    if plant
+      plant.destroy
+      head :no_content
+      
+    else
+      render json: {error: "Could not find plant"}, status: :not_found
+    end
+  end
   private
 
   def plant_params
